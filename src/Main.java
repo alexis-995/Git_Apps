931ec346dce82d98394b2402c5af93f692fbd3a6
alexis-995 <alexis_d_07@hotmail.com>
2022-03-02 10:12:14 -0600

    Adding first frame

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu p1 = new Menu();
+
+        p1.setVisible(true);
+    }
+}
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,10 @@
-public class Menu {
+import javax.swing.*;
+
+
+public class Menu extends JFrame {
+    public Menu(){
+        this.setSize(1080, 720);
+
+
+    }
 }
