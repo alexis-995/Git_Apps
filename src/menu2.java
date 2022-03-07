5f4f8af078f8faaeb190469ed5b1d1389765683b
alexis-995 <alexis_d_07@hotmail.com>
2022-03-07 11:48:15 -0600

    Adding second frame...

---
+++ src/menu2.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class menu2 {
+    private JPanel window;
+    private JTextField product_name;
+    private JComboBox product_type;
+    private JTextField cuantity;
+    private JButton savings;
+    private JButton deletes;
+    private JRadioButton radioButton1;
+
+    private  void createUIComponents() {
+
+
+
+    }
+
+
+}
